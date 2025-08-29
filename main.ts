dac0ca7172e2b16ee5a98aa42a85acf9e12fda5c
garfbit <104676660+garfbit@users.noreply.github.com>
2025-08-29 06:39:16 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let photocelda = 0
+basic.forever(function () {
+    photocelda = pins.digitalReadPin(DigitalPin.P0)
+    if (photocelda == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+})
